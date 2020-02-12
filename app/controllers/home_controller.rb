9934ed0d7b128382d5ebf9688df7e7d18aa4dfb0
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-02-12 21:13:10 +0800

    Commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,12 @@ class HomeController < ApplicationController
     @conversations = Conversation.includes(:recipient, :messages)
                                  .find(session[:conversations])
   end
+
+  def map ;end
+
+  def map_coordinates
+    coordinates = User.all
+
+    render json: coordinates
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  get "map", to: "home#map"
+  get "map_coordinates", to: "home#map_coordinates"
+
   resources :conversations, only: [:create] do
     resources :messages, only: [:create]
     member do
