c31b3825c068f440f8d4c142fdb82cd2e8dd6f76
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-03-03 09:54:43 +0800

    Add config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module DjcCourierServiceFleetManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths << Rails.root.join("lib")
+    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.factory_bot true
